718752b115cbd5293cf870a92799ab7b1b4241f2
Teerawut101 <Teerawut.j@outlook.com>
2017-07-16 09:41:50 +0700

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,19 +1,19 @@
 #include <ZX_microWiFi.h>
- ZX_microWiFi mWiFi(Serial);
+ZX_microWiFi mWiFi(Serial);
 void setup() {
- mWiFi.begin();
- char buf[20];
- sprintf(buf,"temp:%d,val:%d",10,9);
- mWiFi.feed("myFeed0",buf);
- mWiFi.feed("myFeed1","temp:12.5");
- mWiFi.chat("stled1",12.345);
- mWiFi.chat("stled1",12345);
- mWiFi.chat("stled1","12345");
- mWiFi.notify("helloTest");
+  mWiFi.begin();
+  char buf[20];
+  sprintf(buf,"temp:%d,val:%d",10,9);
+  mWiFi.feed("myFeed0",buf);
+  mWiFi.feed("myFeed1","temp:12.5");
+  mWiFi.chat("stled1",12.345);
+  mWiFi.chat("stled1",12345);
+  mWiFi.chat("stled1","12345");
+  mWiFi.notify("helloTest");
 }
 
 void loop() {
- if(mWiFi.ready()){
+  if(mWiFi.ready()){
     mWiFi.println(mWiFi.getMsg());
  }
 }
